c53fe54c6d99373d4110d5a3553aa3abddc32191
Igor Belov <igooor.ww@gmail.com>
2023-02-22 12:33:43 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# PolyglotRocks
+
+PolyglotRocks is a tool that simplifies the localization process for your iOS mobile app. By dropping in our SDK into your project and running the build, you can get AI translations instantly and manual ones a bit later.
+
+## Installation
+
+To install PolyglotRocks, add the following line to your Podfile:
+
+```ruby
+pod 'PolyglotRocks'
+```
+
+Then, run `pod install` to install the library.
+
+To use PolyglotRocks in your Xcode project, add the following command to the build phase:
+
+```plain
+"${PODS_ROOT}/PolyglotRocks/polyglot" <your token>
+```
+
+Replace `<your token>` with the API token provided by PolyglotRocks.
+
+## License
+
+**PolyglotRocks** is released under the MIT license. See [LICENSE](./LICENSE) for details.
---
+++ Tests/translate.spec.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+
+languages=(en fr de)
+app_name='TestApp'
+script='../PolyglotRocks/polyglot'
+file_name='Localizable.strings'
+other_source='Other.strings'
+tenant_token='f233f89a-7868-4e53-854c-9fa60f5b283e'
+translations_path="../$app_name/Extra"
+api_url='https://api.dev.polyglot.rocks'
+product_id='test.bash.app'
+base_file="$translations_path/en.lproj/$file_name"
+export PROJECT_NAME='test.app'
+
+local_env_init() {
+    rm -rf "$translations_path"
+
+    for lang in ${languages[@]}; do
+        path="$translations_path/$lang.lproj";
+        mkdir -p "$path";
+        echo "" > "$path/$file_name"
+        echo "" > "$path/$other_source"
+    done
+
+    rm -rf "/tmp/$PROJECT_NAME"
+    echo '"Cancel" = "Cancel";
+// some comment
+"Saved successfully" = "Saved successfully";
+"4K" = "4K";
+"Loading" = "Loading...";' > $base_file
+    echo '"Loading" = "Loading...";' > "$translations_path/fr.lproj/$file_name"
+    echo '"4K" = "4K";' > "$translations_path/fr.lproj/$file_name"
+    echo '"Loading" = "custom-translation";' > "$translations_path/de.lproj/$file_name"
+}
+
+clear_db() {
+    curl -X DELETE -H "Accept: application/json" -H "Authorization: Bearer $tenant_token" -L "$api_url/products/$1/strings/Cancel" -s >> /dev/null
+    curl -X DELETE -H "Accept: application/json" -H "Authorization: Bearer $tenant_token" -L "$api_url/products/$1/strings/Loading" -s >> /dev/null
+    curl -X DELETE -H "Accept: application/json" -H "Authorization: Bearer $tenant_token" -L "$api_url/products/$1/strings/Saved successfully" -s >> /dev/null
+}
+
+add_manual_translations() {
+    curl -X PUT -H "Content-Type: application/json" -H "Accept: application/json" -H "Authorization: Bearer $tenant_token" -L "$api_url/products/$product_id/strings/Cancel" -d "{ \"translations\": { \"en\": \"Cancel\", \"de\": \"de-manual-test\", \"fr\": \"fr-manual-test\" } }" -s >> /dev/null
+}
+
+setup_suite() {
+    clear_db "$product_id"
+    local_env_init
+}
+
+setup() {
+    export PRODUCT_BUNDLE_IDENTIFIER=$product_id
+    export PROJECT_NAME='test.app'
+}
+
+test_token_not_specified() {
+    assert_equals "`$script`" 'Tenant token is required as a first argument'
+}
+
+test_product_id_not_specified() {
+    export PRODUCT_BUNDLE_IDENTIFIER=''
+    result=`$script $tenant_token ../$app_name`
+    assert_equals "$result" 'Product id is not specified. Use $PRODUCT_BUNDLE_IDENTIFIER for this'
+}
+
+test_invalid_tenant_token() {
+    assert_equals "`$script 11111 ../$app_name`" 'Invalid tenant token'
+}
+
+test_clear_cache_error() {
+    export PROJECT_NAME=''
+    result=`$script --clear-cache`
+    assert_equals "$result" 'Project name is not specified. Set $PROJECT_NAME or pass "./" to it'
+}
+
+test_clear_cache() {
+    add_manual_translations
+    output=`$script $tenant_token ../$app_name`
+    output=`$script --clear-cache`
+    cache_content=`cat "/tmp/$PROJECT_NAME/.last_used_manual_translations"`
+    assert_equals "$cache_content" '{}'
+}
+
+test_found_duplicates() {
+    base_file_content=`cat $base_file`
+    echo '"Loading" = "Loading...";' >> $base_file
+    output="`$script $tenant_token ../$app_name`"
+    is_found_duplicates=`echo "$output" | grep -c 'Found duplicates'`
+    assert_equals $is_found_duplicates 1
+    echo "$base_file_content" > $base_file
+}
+
+test_auto_translation() {
+    setup_suite
+    bash $script $tenant_token ../$app_name
+    translation=`grep 'Cancel' $translations_path/de.lproj/$file_name | cut -d '=' -f 2`
+    custom_translation=`grep 'Loading' $translations_path/de.lproj/$file_name | cut -d '=' -f 2`
+    marked_translation=`grep '4K' $translations_path/fr.lproj/$file_name | cut -d '=' -f 2`
+    assert_equals $translation '"Absagen";'
+    assert_equals ' "4K"; //' "$marked_translation"
+    assert_equals $custom_translation '"custom-translation";'
+}
+
+test_load_manual_translations() {
+    add_manual_translations
+
+    if [ -z "$1" ]; then
+        local_env_init
+    fi
+
+    output=`$script $tenant_token ../$app_name`
+    translation=`grep 'Cancel' $translations_path/de.lproj/$file_name | cut -d '=' -f 2`
+    assert_equals $translation '"de-manual-test";'
+}
+
+test_replace_auto_translations_with_manual() {
+    setup_suite
+    test_auto_translation
+    test_load_manual_translations 'no-clear'
+}
+
+test_translations_from_other_file() {
+    cp $base_file $translations_path/en.lproj/$other_source
+    echo '"Now" = "Now";' >> $translations_path/en.lproj/$other_source
+
+    output=`$script $tenant_token ../$app_name $other_source`
+    translation=`grep 'Now' $translations_path/fr.lproj/$other_source | cut -d '=' -f 2`
+    assert_equals "$translation" ' "À présent";'
+}
+
+test_do_nothing_without_updates() {
+    output=`$script $tenant_token ../$app_name`
+    output=`$script $tenant_token ../$app_name`
+    output="`$script $tenant_token ../$app_name`"
+    translation_count=`grep -c 'Loading' $translations_path/de.lproj/$file_name`
+    files_without_changes=`echo "$output" | grep -c 'seems to be translated already'`
+    assert_equals $files_without_changes 2
+    assert_equals $translation_count 1
+}
