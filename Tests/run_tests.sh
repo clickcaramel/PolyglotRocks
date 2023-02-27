ae24b0d635bb8fff3086cedbc4907f4877d3f317
Igor Belov <igooor.ww@gmail.com>
2023-02-27 14:50:35 +0300

    fix: polyglot test suite

--- Tests/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-bash <(curl -s https://raw.githubusercontent.com/pgrange/bash_unit/master/install.sh)
-tests=`find ./Tests/*.spec.sh`
-
-for path in ${tests[@]}; do
-    ./bash_unit $path
-done
